842741fb3340cc7d27e3ad0596db466f8e91d0c7
varsha633 <dulari150504@gmail.com>
2026-06-11 11:49:29 +0530

    Create temperature.py

---
+++ day1/temperature.py
@@ -0,0 +1,6 @@
+celsius=float(input("enter tempersture in celcius :"))
+fahrenheit=(celsius *9/5)+32
+print("temperature in fahrenheit =",fahrenheit)
+fahrenheit=float(input("enter temperature in fahrenheit :"))
+celsius =(fahrenheit -32)*5/9
+print("temperature in celsius =",celsius)
