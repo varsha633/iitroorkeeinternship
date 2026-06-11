219637c54488cd1a8ca8bf5298f93d138e1d6f6d
varsha633 <dulari150504@gmail.com>
2026-06-11 11:47:44 +0530

    Create simplecalculator.py

---
+++ day1/simplecalculator.py
@@ -0,0 +1,9 @@
+num1=float(input("enter first number: "))
+num2=float(input("enter second number: "))
+print("addition=",num1+num2)
+print ("subtraction=",num1-num2)
+print("multiplication",num1*num2)
+if num2!=0:
+    print("division=",num1/num2)
+else:
+    print("division by zero is not allowed.")
