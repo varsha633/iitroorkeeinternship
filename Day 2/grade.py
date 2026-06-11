c4ad1076f44bdfc9eea2b95c79c7b92541f676c9
varsha633 <dulari150504@gmail.com>
2026-06-11 12:14:39 +0530

    Update grade.py

--- Day 2/grade.py
+++ Day 2/grade.py
@@ -1,1 +1,11 @@
-
+a=int(input("enter marks:"))
+if a>=90:
+    print("grade :A")
+elif a>=80:
+    print("grade:B")
+elif a>=70:
+    print("grade:C")
+elif a>=60:
+    print("grade:D")
+else:
+    print("grade:F")
