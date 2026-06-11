ae8923f079ef854eca5243e298ed0837590b3845
varsha633 <dulari150504@gmail.com>
2026-06-11 12:35:40 +0530

    Update guessnumber.py

--- Day 2/guessnumber.py
+++ Day 2/guessnumber.py
@@ -1,1 +1,12 @@
+secret_number = 7
+guess = 0
 
+while guess != secret_number:
+    guess = int(input("Guess the number (1-10): "))
+
+    if guess < secret_number:
+        print("Too low!")
+    elif guess > secret_number:
+        print("Too high!")
+    else:
+        print("Congratulations! You guessed it.")
