e884c61a35d2b0477c6b3b74522e5fa272505bf9
varsha633 <dulari150504@gmail.com>
2026-06-14 12:10:31 +0530

    Update fibonacci.py

--- day3/fibonacci.py
+++ day3/fibonacci.py
@@ -1,1 +1,11 @@
-
+def fibo(n):
+    if n==1:
+      return 0
+    elif n==2:
+      return 1 
+    else:
+        return fibo(n-1)+fibo(n-2)
+n =int(input("enter number:"))
+for i in range (1,n+1):
+        f=fibo(i)
+        print(f)
