5797a5e7b4ad25c79207b85331ba1709528761f4
varsha633 <dulari150504@gmail.com>
2026-06-14 17:37:16 +0530

    Update factorial.py

--- day3/factorial.py
+++ day3/factorial.py
@@ -1,1 +1,14 @@
-
+#iterative approach
+num=int(input("enter a number:"))
+fact=1
+for i in range(1,num+1):
+    fact*=i
+print("Factorial =",fact)
+ #recursive approach
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+num=int(input("enter a number:"))
+print("factorial=",factorial)
