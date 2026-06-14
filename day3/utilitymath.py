c9ecc587cc738066d0876889e44372306866afa2
varsha633 <dulari150504@gmail.com>
2026-06-14 12:27:34 +0530

    Update utilitymath.py

--- day3/utilitymath.py
+++ day3/utilitymath.py
@@ -1,1 +1,10 @@
-
+import random
+def roll_dice():
+    return random.randint(1,6)
+while True:
+    user_input=input("enter to roll dice")
+    if user_input.lower()=="q":
+        print("thank for playing!")
+        break
+    else:
+     print("you rolled:",roll_dice())
