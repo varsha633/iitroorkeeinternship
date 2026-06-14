1cd315909b8416a4287a69cdcc75c482c63d6b7d
varsha633 <dulari150504@gmail.com>
2026-06-14 12:50:38 +0530

    Update utilitymath.py

--- day3/utilitymath.py
+++ day3/utilitymath.py
@@ -1,1 +1,17 @@
-
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    if b!=0:
+        return a/b
+    return"cannot divide by zero"
+def square(a):
+        return a*a
+print(add(10,5))
+print(subtract(10,5))
+print(multiply(10,5))
+print(divide(10,5))
+print(square(10))
